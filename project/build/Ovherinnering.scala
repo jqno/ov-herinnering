3ef155e39efe0e0d9e6f6837126b940f76e148db
Jan Ouwens <jan.ouwens@gmail.com>
2011-06-28 23:02:23 +0200

    GPL'ed

--- project/build/Ovherinnering.scala
+++ project/build/Ovherinnering.scala
@@ -1,3 +1,24 @@
+/*
+ * OV-Herinnering is an Android app that asks for a train station,
+ * and gives a notification when it is reached.
+ *
+ * Copyright (C) 2011 by Jan Ouwens
+ *
+ * This file is part of OV-Herinnering.
+ *
+ * OV-Herinnering is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OV-Herinnering is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OV-Herinnering.  If not, see <http://www.gnu.org/licenses/>.
+ */
 import sbt._
 
 trait Defaults {
