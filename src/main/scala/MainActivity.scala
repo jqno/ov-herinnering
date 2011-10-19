f71e87da2a0b2eba3917d51eaaaf59a670fc0778
Jan Ouwens <jan.ouwens@gmail.com>
2011-10-20 00:14:41 +0200

    Replaced Service with AlarmManager

--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -21,51 +21,92 @@
  */
 package nl.jqno.ovherinnering
 
-import android.app.Activity
+import android.app._
 import android.content._
+import android.content.Context._
 import android.os._
 import android.view.View
 import android.widget._
+import java.util.Calendar
 import FindView._
 import Station._
 
 class MainActivity extends Activity with FindView {
   var station: Option[String] = None
+  var active = false
+
+  private val ACTIVE_ID = "active"
+  private val NOTIFICATION_ID = 1337
+  private val REQUEST_CODE = 12345
+  private val INTERVAL_MINUTE = 60000
 
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main)
 
-    startAndBindLocationService
     initializeAutoCompleter
     initializeEventHandlers
+  }
+
+  override def onResume {
+    super.onResume
+
+    val prefs = getPreferences(MODE_PRIVATE)
+    active = prefs.getBoolean(ACTIVE_ID, false)
+    toggleUi
+  }
+
+  override def onPause {
+    super.onPause
+
+    val editor = getPreferences(MODE_PRIVATE).edit
+    editor.putBoolean(ACTIVE_ID, active)
+    editor.commit
   }
 
   def start {
     val city = findView[EditText](R.id.main_city).getText.toString
     if (STATIONS contains city) {
-      locationService foreach { s =>
-        station = Some(city)
-        findView[TextView](R.id.main_text) setText city
-        s.activate
-        toggleUi
-      }
+      findView[TextView](R.id.main_text) setText city
+      scheduleAlarm(Some(city))
+      active = true
+      toggleUi
     }
     else
       Toast.makeText(this, "Hela wel iets fatsoenlijks intikken hoor.", Toast.LENGTH_LONG).show
   }
 
   def stop {
-    locationService foreach { s =>
-      s.deactivate
-      toggleUi
-    }
+    cancelAlarm
+    active = false
+    toggleUi
   }
 
-  def startAndBindLocationService {
-    val serviceIntent = new Intent(this, classOf[LocationService])
-    startService(serviceIntent)
-    bindService(serviceIntent, connection, Context.BIND_AUTO_CREATE)
+  def scheduleAlarm(city: Option[String]) {
+    val cal = Calendar.getInstance
+    cal.add(Calendar.SECOND, 5)
+    alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis, INTERVAL_MINUTE, broadcastIntent(city))
+    notificationManager.notify(NOTIFICATION_ID, notification)
+  }
+
+  def cancelAlarm {
+    alarmManager.cancel(broadcastIntent(None))
+    notificationManager.cancel(NOTIFICATION_ID)
+  }
+
+  def notification: Notification = {
+    val text = getText(R.string.notification_active)
+    val notification = new Notification(R.drawable.app_icon, text, System.currentTimeMillis)
+    val intent = PendingIntent.getActivity(this, 0, new Intent(this, classOf[MainActivity]), 0)
+    notification.setLatestEventInfo(this, text, text, intent)
+    notification.flags |= Notification.FLAG_ONGOING_EVENT | Notification.FLAG_NO_CLEAR
+    return notification
+  }
+
+  def broadcastIntent(city: Option[String]) = {
+    val intent = new Intent(this, classOf[AlarmReceiver])
+    city foreach (intent.putExtra("station", _))
+    PendingIntent.getBroadcast(this, REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT)
   }
 
   def initializeAutoCompleter =
@@ -76,23 +117,14 @@ class MainActivity extends Activity with FindView {
     findView[Button](R.id.main_stop).onClick { _ => stop }
   }
 
+  private def alarmManager: AlarmManager =
+    getSystemService(ALARM_SERVICE).asInstanceOf[AlarmManager]
+
+  private def notificationManager: NotificationManager =
+    getSystemService(NOTIFICATION_SERVICE).asInstanceOf[NotificationManager]
+
   def toggleUi {
-    val active = locationService map (_.isActive) getOrElse false
     find(R.id.main_active)   setVisibility (if (active) View.VISIBLE else View.GONE)
     find(R.id.main_inactive) setVisibility (if (active) View.GONE else View.VISIBLE)
-  }
-
-  private var locationService: Option[LocationService] = None
-
-  val connection = new ServiceConnection {
-    override def onServiceConnected(className: ComponentName, binder: IBinder) {
-      locationService = Some(binder.asInstanceOf[LocationService#LocalBinder].getService)
-      toggleUi
-    }
-
-    override def onServiceDisconnected(className: ComponentName) {
-      locationService = None
-      toggleUi
-    }
   }
 }
